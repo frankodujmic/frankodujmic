3133612641a6085820a9d443099c24292c8b6d4f
plasticpony <izabela.bratovic@gmail.com>
2015-03-29 19:28:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿var images = ['http://frankodujmic.github.io/images/fox-proba4.jpg',
+var images = ['http://frankodujmic.github.io/images/fox-proba4.jpg',
     'http://frankodujmic.github.io/images/kaktus-scena3.jpg',
     'http://frankodujmic.github.io/images/zecov.jpg',
     'http://frankodujmic.github.io/images/maczka.jpg'];
@@ -30,18 +30,19 @@ $(document).ready(function () {
 
 function next() {
 
-    //if (navigator.product == 'Gecko') {
-    //    for (i = 0; i <= 5; i++) {
-    //        if ($("#rightPhoto").css("background-image") == 'url("' + picsRight[i] + '")') {
-    //            if (i == 4) {
-    //                $("#rightPhoto").css("background-image", 'url("' + picsRight[0] + '")');
-    //                break;
-    //            }
-    //            $("#rightPhoto").css("background-image", 'url("' + picsRight[i + 1] + '")');
-    //            break;
-    //        }
-    //    }
-    //}
+    if (navigator.product == 'Gecko') {
+        for (i = 0; i <= 5; i++) {
+            if ($("#image").css("background-image") == 'url("' + images[i] + '")') {
+                if (i == 4) {
+                    $("#image").css("background-image", 'url("' + images[0] + '")');
+                    break;
+                }
+                $("#image").css("background-image", 'url("' + images[i + 1] + '")');
+                break;
+            }
+        }
+    }
+
     for (i = 0; i <= images.length; i++) {
         if ($("#image").css("background-image") == 'url(' + images[i] + ')') {
             if (i == images.length - 1) {
