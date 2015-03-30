70fdbc478a9d88dfe0a85251736da020413ccfd8
plasticpony <izabela.bratovic@gmail.com>
2015-03-30 21:31:02 +0200

    arrow, about

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var images = ['http://frankodujmic.github.io/images/fox-proba4.jpg',
-    'http://frankodujmic.github.io/images/kaktus-scena3.jpg',
-    'http://frankodujmic.github.io/images/zecov.jpg',
-    'http://frankodujmic.github.io/images/maczka.jpg'];
+var images = ['http://lowpolypessimism.github.io/images/fox-proba4.jpg',
+    'http://lowpolypessimism.github.io/images/kaktus-scena3.jpg',
+    'http://lowpolypessimism.github.io/images/zecov.jpg',
+    'http://lowpolypessimism.github.io/images/maczka.jpg'];
 
 $(document).ready(function () {
     var defaults = {
@@ -25,15 +25,36 @@ $(document).ready(function () {
         next();
         $.adaptiveBackground.run(defaults);
     });
+
+    $('#left').click(function () {
+        previous();
+        $.adaptiveBackground.run(defaults);
+    });
+
+    document.onkeydown = checkKey;
+
+    function checkKey(e) {
+
+        e = e || window.event;
+
+        if (e.keyCode == '37') {
+            previous();
+            $.adaptiveBackground.run(defaults);
+        }
+        else if (e.keyCode == '39') {
+            next();
+            $.adaptiveBackground.run(defaults);
+        }
+    }
 });
 
 
 function next() {
 
     if (navigator.product == 'Gecko') {
-        for (i = 0; i <= 5; i++) {
+        for (i = 0; i <= images.length; i++) {
             if ($("#image").css("background-image") == 'url("' + images[i] + '")') {
-                if (i == 4) {
+                if (i == images.length - 1) {
                     $("#image").css("background-image", 'url("' + images[0] + '")');
                     break;
                 }
@@ -50,6 +71,33 @@ function next() {
                 break;
             }
             $("#image").css("background-image", 'url(' + images[i + 1] + ')');
+            break;
+        }
+    }
+};
+
+function previous() {
+
+    if (navigator.product == 'Gecko') {
+        for (i = 0; i <= images.length; i++) {
+            if ($("#image").css("background-image") == 'url("' + images[i] + '")') {
+                if (i == 0) {
+                    $("#image").css("background-image", 'url("' + images[images.length - 1] + '")');
+                    break;
+                }
+                $("#image").css("background-image", 'url("' + images[i - 1] + '")');
+                break;
+            }
+        }
+    }
+
+    for (i = 0; i <= images.length; i++) {
+        if ($("#image").css("background-image") == 'url(' + images[i] + ')') {
+            if (i == 0) {
+                $("#image").css("background-image", 'url(' + images[images.length - 1] + ')');
+                break;
+            }
+            $("#image").css("background-image", 'url(' + images[i - 1] + ')');
             break;
         }
     }
